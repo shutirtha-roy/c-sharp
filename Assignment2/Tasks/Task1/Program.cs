cb59dea5597cef44ea0c44de59e4310b67f880d7
Shutirtha Roy <shutirtha1998@gmail.com>
2021-08-12 05:38:37 +0600

    Assignment 2

--- Assignment2/Tasks/Task1/Program.cs
+++ Assignment2/Tasks/Task1/Program.cs
@@ -12,25 +12,18 @@ namespace Task1
 
         public static bool IsPrime(int number)
         {
-            if ((number >= 2 && number <= 1000000))
+            var count = 0;
+            for (var i = 1; i <= number; i++)
             {
-                var count = 0;
-                for (var i = 1; i <= number; i++)
+                if (number % i == 0)
                 {
-                    if (number % i == 0)
-                    {
-                        count++;
-                    }
+                    count++;
                 }
+            }
 
-                if (count == 2)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+            if (count == 2)
+            {
+                return true;
             }
             else
             {
